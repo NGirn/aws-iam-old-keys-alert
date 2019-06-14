cb7c58019483c537281a3fbbdf8691be913db5de
Nav <navdeepsinghgirn@gmail.com>
2019-06-14 18:44:56 +0100

    Everything now works perfectly.

--- index.js
+++ index.js
@@ -1,100 +1,57 @@
+'use strict';
+
 // Global Variables
 const AWS = require('aws-sdk');
-const ARN = 'arn:aws:sns:us-east-1:317241229763:oldAccessKeyAlert';
-var TTL = 1; // In days
-var REGION_NAME = 'us-east-1';
+const TTL = 1;
+const ARN = '';
 
-// This runs when event is triggered
-exports.handler = async (event) => {
-    await getOldKeys();
-};
+module.exports.hello = async (event) => {
+  await getOldKeys();
+  };
 
 // Function to convert date format into unix timestamp
 async function toTimestamp(strDate){
-    var datum = Date.parse(strDate);
-    return datum/1000;
+  var datum = Date.parse(strDate);
+  return datum/1000;
 }
 
-
 async function getOldKeys(){
-    // Get the list of users
-    var iam = await new AWS.IAM();
+  // Get the list of users
+  var iam = await new AWS.IAM();
+  var params = {};
+  var users = await iam.listUsers(params).promise();
+  
+  var oldKeyUsers = [];
+  
+  //Iterate through list of users to access each users' data
+  var arrayLength = users.Users.length;
+  for (var i=0; i < arrayLength; i++) {
     var params = {
-      };
-    var users = await iam.listUsers(params);
+      UserName: users.Users[i].UserName
+    };
+    var accessKeyData = await iam.listAccessKeys(params).promise();
+    var dateCreated = accessKeyData.AccessKeyMetadata[0].CreateDate;
+    dateCreated = Date.parse(dateCreated);
+    var tsCreated = dateCreated/1000
     
-    console.log(users);
-    var oldKeyUsers = [];
-
-    // Iterate through list of users to access each users' data
-    var arrayLength = users.length;
-    for (var i = 0; i < arrayLength; i++) {
-        var params = {
-            UserName: users[i]
-           };
-        var accessKey = [];
-        var accessKeyData = await iam.listAccessKeys(params, function(err, data) {
-            if (err) console.log(err, err.stack); // an error occurred
-            else     console.log(data);           // successful response
-            /*
-            data = {
-             AccessKeyMetadata: [
-            {
-                AccessKeyId: "AKIA111111111EXAMPLE", 
-                CreateDate: <Date Representation>, 
-                Status: "Active", 
-                UserName: "Alice"
-            }, 
-                 {
-                AccessKeyId: "AKIA222222222EXAMPLE", 
-                CreateDate: <Date Representation>, 
-                Status: "Active", 
-                UserName: "Alice"
-               }
-              ]
-             }
-             */
-           });
-        var tsKey;
-        tsKey = accessKeyData.AccessKeyMetadata.CreateDate;
-        tsKey = toTimestamp(tsKey);
-        
-        // Creates todays' date in a unix timestamp format
-        var today = new Date();
-        var dd = String(today.getDate()).padStart(2, '0');
-        var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-        var yyyy = today.getFullYear();
-
-        today = mm + '/' + dd + '/' + yyyy;
-        var tsToday = toTimestamp(today);
-        var threshold = tsToday - (TTL*86400) 
-
-        // Compares timestamps for when the keys were created and today's timestamp minus the required TTL (time to live) of the access key
-        if (tsKey <=  threshold) {
-            // Adds user to a list of users required to rotate their access keys
-            var username;
-            username = accessKeyData.AccessKeyMetadata.UserName;
-            oldKeyUsers.push(username);
-       }
+    //Creates today's date in a unix timestamp format
+    var tsToday = Date.now() / 1000 | 0
+    var threshold = tsToday - (TTL*86400)
+    
+    //Identifies which users are in need of rotating their keys
+    if (tsCreated < threshold) {
+      var usernameToRotate = accessKeyData.AccessKeyMetadata[0].UserName;
+      oldKeyUsers.push(usernameToRotate);
     }
 
-    // Sending an SNS push notifcation of all users in the list that are required to rotate thier access keys
-    var sns = new AWS.SNS();
-    var params = {
-        Message: 'oldKeyUsers', /* required */
-        MessageAttributes: {
-          '<String>': {
-            DataType: 'String.Array', /* required */
-          },
-          /* '<String>': ... */
-        },
-        Subject: 'Users That Are Required To Change Their Access Keys',
-        TopicArn: ARN
-      };
-    await sns.publish(params, function(err, data) {
-        if (err) console.log(err, err.stack); // an error occurred
-        else     console.log(data);           // successful response
-    });
-
-    return oldKeyUsers;    
+  }
+  oldKeyUsers = oldKeyUsers.toString();
+  //Sends SNS notification message containing a list of all the users that are required to rotate their access ID keys
+  var sns = await new AWS.SNS();
+  var params = {
+      Message: oldKeyUsers, /* required */
+      Subject: 'Users That Are Required To Change Their Access Keys',
+      TopicArn: ARN
+    };
+  await sns.publish(params).promise();
 }
